003b813bb2ca29a1da5b9e376ecf235ceca51993
Lazar Ristic <lazar.ristic@vibe.rs>
2021-12-27 13:40:35 +0100

    Add test requirements

--- test/cashier_test.exs
+++ test/cashier_test.exs
@@ -2,7 +2,19 @@ defmodule CashierTest do
   use ExUnit.Case
   doctest Cashier
 
-  test "greets the world" do
-    assert Cashier.hello() == :world
+  test "add green tea, strawberry, green tea, green tea, coffee in basket, total of the basket returns £22.45" do
+    assert "£22.45" = false
+  end
+
+  test "add green tea, green tea in basket, total of the basket returns £3.11" do
+    assert "£3.11" = false
+  end
+
+  test "add strawberry, strawberry, green tea, strawberry in basket, total of the basket returns £16.61" do
+    assert "£16.61" = false
+  end
+
+  test "add green tea, coffee, strawberry, coffee, coffee in basket, total of the basket returns £30.57" do
+    assert "£30.57" == false
   end
 end
